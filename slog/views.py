da0aee348eaba85712b86974a18fc42cedd899ca
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2015-07-28 18:20:40 +1000

    moved to flask-bootstrap; using flask-wtf; testing on options page...

--- slog/views.py
+++ slog/views.py
@@ -1,5 +1,9 @@
 from flask import render_template,session,redirect,url_for,request,current_app,make_response
 
+from flask.ext.wtf import Form
+from wtforms import StringField, SelectField, SelectMultipleField, SubmitField
+from wtforms.validators import DataRequired
+
 from slog import app
 from config import data
 from slog.models import ElogGroupData,ElogGroups,ElogData,SolUsers
@@ -11,14 +15,28 @@ from datetime import datetime
 from avatar_generator import Avatar
 
 
+
 # desired paths /<groupUrl>/<year>/<month>/<day>
 @app.route('/')
 def slog():
     return render_template('index.html')
 
-@app.route('/options/')
+class OptionsForm(Form):
+    group_choices = [('1','Operators'),('2','IR'),('3','Far-IR')]
+    page_choices = [('1','25'),('2','50'),('3','75'),('4','100'),('5','ALL')]
+    nickname = StringField('Nickname:', validators=[DataRequired()])
+    default_group = SelectField('Default Group:', choices=group_choices, validators=[DataRequired()])
+    view_gropus = SelectMultipleField('View Groups:', choices=group_choices, validators=[DataRequired()])
+    entries_per_page = SelectField('Entries per Page:', choices=page_choices, validators=[DataRequired()])
+    # submit = SubmitField('Save Changes') #needed in footer of panel
+    pass
+
+@app.route('/options/', methods=['GET', 'POST'])
 def options():
-    return render_template('options.html')
+    form = OptionsForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('options.html', form=form)
 
 #https://github.com/maethor/avatar-generator
 @app.route('/avatar.png')
